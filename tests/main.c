56a6b53921a061b165e7618f35200a061a65b54d
Ning Shi <zeegeek@gmail.com>
2009-09-15 18:33:40 -0400

    Adds monitoring test.

--- tests/main.c
+++ tests/main.c
@@ -13,12 +13,20 @@ gboolean test_handler(falcon_object_t *object, falcon_event_code_t event) {
 }
 
 int main(int argc ATTRIBUTE_UNUSED, char **argv) {
+	GMainLoop *ml = NULL;
+
 	g_thread_init(NULL);
 	falcon_init();
+
 	falcon_set_log_level(G_LOG_LEVEL_MESSAGE);
 	falcon_handler_register(EVENT_DIR_CREATED, test_handler);
 
-	falcon_add(argv[1], FALSE);
+	falcon_add(argv[1], TRUE);
+
+	ml = g_main_loop_new (NULL, FALSE);
+
+	g_main_loop_run(ml);
+	g_main_loop_unref(ml);
 	falcon_shutdown(TRUE);
 
 	return 0;
