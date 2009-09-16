e993b76b8efd3d1b6f515c44157aa13797900d68
Ning Shi <zeegeek@gmail.com>
2009-09-15 21:23:54 -0400

    Uses the new custom handler signature.

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,8 @@
 #include "events.h"
 #include "common.h"
 
-gboolean test_handler(falcon_object_t *object, falcon_event_code_t event) {
+gboolean test_handler(falcon_object_t *object, falcon_event_code_t event,
+                      gpointer userdata ATTRIBUTE_UNUSED) {
 	g_message("custom handler: %s->%s",
 	          object->name, falcon_event_to_string(event));
 	return TRUE;
@@ -19,7 +20,7 @@ int main(int argc ATTRIBUTE_UNUSED, char **argv) {
 	falcon_init();
 
 	falcon_set_log_level(G_LOG_LEVEL_MESSAGE);
-	falcon_handler_register(EVENT_ALL, test_handler);
+	falcon_handler_register(EVENT_ALL, test_handler, NULL);
 
 	falcon_add(argv[1], TRUE);
 
